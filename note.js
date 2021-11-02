ca7cd0a60a83417a95eeb61d8c699d1dba9ea869
Aldin <alhuric89@gmail.com>
2021-11-02 17:58:38 +0100

    add find method

--- note.js
+++ note.js
@@ -1,4 +1,17 @@
-const notes = ["Note 1", "Note 2", "Note 3"];
+const notes = [
+    {
+        title:"Note 1",
+        body:"I would like to go to Spain"
+    },
+    {
+        title:"Note 2",
+        body:"Exercise. Eatiing a bit better"
+     },
+     { 
+         title:"Note 3",
+        body:"Get a new seat"
+     }
+    ];
 /*console.log(notes);
 console.log(notes.length);
 console.log(notes[0]);
@@ -11,3 +24,20 @@ console.log(notes);
 
 
 notes.indexOf("Note 2");
+
+const findNotes = function (note, query) {
+    return note.filter((e,index)=>{
+        const isTitleMatch = e.title.toLowerCase().includes(query.toLowerCase());
+        const isBodyMatch = e.body.toLowerCase().includes(query.toLowerCase());
+        return isTitleMatch || isBodyMatch; 
+        })
+}
+
+/*
+console.log(findNotes(notes, "Note 1"));
+//filter doesnt edit original array
+const filteredNotes = notes.filter((e,index)=>{
+const isTitleMatch = e.title.toLowerCase().includes("ne");
+const isBodyMatch = e.body.toLowerCase().includes("ne");
+return isTitleMatch || isBodyMatch; 
+})*/
