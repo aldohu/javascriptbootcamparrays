6d4057bccc085938b4f8e59ba871da578986ba89
Aldin <alhuric89@gmail.com>
2021-11-02 18:35:20 +0100

    add filter function to todo.js

--- todo.js
+++ todo.js
@@ -21,33 +21,35 @@ const toDo = [
     }
 
 ];
-
+/*
 const deleteTodo = function(arr, prop) {
    const index = arr.findIndex(e=> e.text.toLowerCase() == prop.toLowerCase());
    arr.splice(index, 1);
-}
+}*/
 
 //1. convert array of objects  text and completed prop
 
 //2. create function to remove todo by text value
 
+//sort edit original array and dont return anything
 
 
 //find todos with completed = true value
 
-
-const getThingsTodo = (todo) => {
+const sortTodos = notes=> notes.sort((a,b)=> a.completed - b.completed)
+console.log(sortTodos(toDo));
+/*const getThingsTodo = (todo) => {
    return todo.filter(e=>
      e.completed === false)
         
     
 
-}
+}*/
 //deleteTodo(toDo, 'sleep')
 
 //console.log(toDo);
 
-console.log(getThingsTodo(toDo))
+//console.log(getThingsTodo(toDo))
 
 
 
